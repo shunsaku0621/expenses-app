5d94d4f55ef97ee17b56b10e5eaece8d30eb29a4
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-24 22:53:19 +0900

    simple calendarの日付取得

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,6 +2,7 @@ class ExpensesController < ApplicationController
   def index
     @incomes = Income.order("created_at")
     @income_sum = @incomes[0].income_sum
+    my_page_show
   end
 
   def new
@@ -21,5 +22,10 @@ class ExpensesController < ApplicationController
   private
   def income_params
     params.require(:income).permit(:amount, :memo, :category_id, :start_time).merge(user_id: current_user.id)
+  end
+
+
+  def my_page_show
+    @day_params = params[:start_date]
   end
 end
