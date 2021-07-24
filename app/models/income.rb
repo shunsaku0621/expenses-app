7009f0725e5a4083769212acdc00fb9814a3404b
shunsaku0621 <hamagi.0621@icloud.com>
2021-07-24 21:35:35 +0900

    カレンダーに金額表示

--- app/models/income.rb
+++ app/models/income.rb
@@ -5,4 +5,16 @@ class Income < ApplicationRecord
 
 
   validates :category_id, numericality: { other_than: 1 }
+
+
+
+
+  def income_sum
+    income_array = []
+    Income.all.each do |income|
+      income_array << income.amount
+    end
+    income_array.sum
+  end
+  
 end
